0009fb2a2d8fd336a30cc1ec78026236c707bb06
Mohamad Yassine <104362697+0x-yassine@users.noreply.github.com>
2024-06-21 20:06:29 +0300

    update

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ const aboutSection = document.getElementById('about');
 button.addEventListener('click', () => {
     aboutSection.scrollIntoView({ behavior: "smooth" });
 });
+
+const abt_btn = document.querySelector('.about-cta');
+const contactSection = document.getElementById('contact');
+
+abt_btn.addEventListener('click', () => {
+    contactSection.scrollIntoView({ behavior: "smooth" });
+})
