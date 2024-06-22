cc89c3eba41b91a327178a146df3b83d49be2391
Mohamad Yassine <104362697+0x-yassine@users.noreply.github.com>
2024-06-22 21:24:15 +0300

    update

--- script.js
+++ script.js
@@ -9,14 +9,32 @@ function toggleMenu() {
 
 const button = document.querySelector('.main-cta');
 const aboutSection = document.getElementById('about');
+const servicesSection = document.getElementById('services');
+const contactSection = document.getElementById('contact');
 
 button.addEventListener('click', () => {
     aboutSection.scrollIntoView({ behavior: "smooth" });
 });
 
 const abt_btn = document.querySelector('.about-cta');
-const contactSection = document.getElementById('contact');
 
 abt_btn.addEventListener('click', () => {
     contactSection.scrollIntoView({ behavior: "smooth" });
+});
+
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector("nav");
+    //const aboutSectionTop = aboutSection.getBoundingClientRect().top;
+    //const servicesSectionTop = servicesSection.getBoundingClientRect().top;
+    //const contactSectionTop = contactSection.getBoundingClientRect().top;
+    //const isPastServices = window.scrollY > servicesSectionTop;
+    //const isPastContact = window.scrollY > contactSectionTop;
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+    //navbar.classList.toggle("black-on-services", isPastServices);
+    //navbar.classList.toggle("white-on-contact", isPastContact);
+});
+
+window.addEventListener("scroll", function () {
+    var mobilenavbar = document.querySelector(".mobile-nav");
+    mobilenavbar.classList.toggle("sticky", window.scrollY > 0);
 });
