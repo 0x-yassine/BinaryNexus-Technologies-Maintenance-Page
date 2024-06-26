3dbef7027a9b5514cbe8605e78502d415804aab8
Mohamad Yassine <104362697+0x-yassine@users.noreply.github.com>
2024-06-26 22:40:35 +0300

    update

--- contactData.php
+++ contactData.php
@@ -6,27 +6,16 @@ $db_name = "binarynexus_technologies_mails";;
 
 $conn = new mysqli($servername, $user, $password, $db_name);
 
-if($conn -> error) {
-    die('Connecttion Failed : '.$conn->error);
+if ($conn->error) {
+    die('Connecttion Failed : ' . $conn->error);
 } else {
-    echo("Connection Succeeded!");
+    if (isset($_POST['emailInput']) && isset($_POST['subjectInput']) && isset($_POST['textInput'])) {
+        $emailInput = $_POST['emailInput'];
+        $subjectInput = $_POST['subjectInput'];
+        $textInput = $_POST['textInput'];
+        $sql = "INSERT INTO `mails` (email, subject, text) VALUES('$emailInput', '$subjectInput', '$textInput')";
+        $conn->query($sql);
+    } else {
+        echo ('Please fill the fields correctly');
+    }
 }
-
-/*
-$email = $_POST['emailInput'];
-$subject = $_POST['subjectInput'];
-$text = $_POST['textInput'];
-
-//Database Connection
-$conn = new mysqli('localhost', 'root', '', 'binarynexus_technologies_mails');
-if ($conn->connect_error) {
-    die('Connection Failed : ' . $conn->connect_error);
-} else {
-    $stmt = $conn->prepare("insert into mails(email, subject, text) values(?,?,?)");
-    $stmt->bind_param("sss", $email, $subject, $text);
-    $stmt->execute();
-    echo '<script>alert("Thanks for contacting us!")</script>';
-    $stmt->close();
-    $conn->close();
-}
-*/
