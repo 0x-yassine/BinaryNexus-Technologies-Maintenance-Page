3e75b376bb78925a778df97bc0fc093baf25785d
Mohamad Yassine <104362697+0x-yassine@users.noreply.github.com>
2024-06-27 10:11:19 +0300

    update

--- contactData.php
+++ contactData.php
@@ -1,21 +1,2 @@
 <?php
-$servername = "localhost";
-$user = "root";
-$password = "";
-$db_name = "binarynexus_technologies_mails";;
-
-$conn = new mysqli($servername, $user, $password, $db_name);
-
-if ($conn->error) {
-    die('Connecttion Failed : ' . $conn->error);
-} else {
-    if (isset($_POST['emailInput']) && isset($_POST['subjectInput']) && isset($_POST['textInput'])) {
-        $emailInput = $_POST['emailInput'];
-        $subjectInput = $_POST['subjectInput'];
-        $textInput = $_POST['textInput'];
-        $sql = "INSERT INTO `mails` (email, subject, text) VALUES('$emailInput', '$subjectInput', '$textInput')";
-        $conn->query($sql);
-    } else {
-        echo ('Please fill the fields correctly');
-    }
-}
+echo "hello";
